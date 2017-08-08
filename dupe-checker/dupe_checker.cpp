b46a690597975daef7d09c34d92f4468444168ba
Juhyeok Sin <homin203@gmail.com>
2017-08-08 06:26:37 -0700

    erase comments first, then redo

---
+++ dupe-checker/dupe_checker.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+string removeSpaces(const string);
+
+int main() {
+  // variables
+  static string temp1, temp2;
+  string text1, text2, fileName1, fileName2; 
+  ifstream fin1, fin2;
+  
+  cout << "Enter the first file name: ";
+  getline(cin, fileName1);
+
+  cout << "Enter the second file name: ";
+  getline(cin, fileName2);
+
+  fin1.open(fileName1);
+  fin2.open(fileName2);
+
+  while (fin1.good() && fin2.good()) {
+    getline(fin1, text1);
+    getline(fin2, text2);
+
+    if ((text1 == "EOF") || (text2 == "EOF")) break;
+    if (text1.empty() && text2.empty()) continue;
+
+    temp1 += removeSpaces(text1);
+    temp2 += removeSpaces(text2);
+  }
+
+  cout << temp1 << endl;
+  cout << endl;
+  cout << temp2 << endl;
+
+  return 0;
+}
+
+/**
+ * special space remover
+ * @param    getUserInput [constant value of string data type value]
+ * @return   temp         [storing a string without spaces]
+ */
+string removeSpaces(const string getUserInput) {
+  string temp; // declare a string data type variable temp
+  for (int i = 0; i < getUserInput.size(); i++) {
+    // if a character of a string is lower case or upper case alphabet,
+    // assign each value into the temp
+        if ((int)getUserInput[i] >= 33 && (int)getUserInput[i] <= 127)
+      temp += getUserInput[i]; // temp = temp + getUserInput[i];
+  }
+  return temp;
+}
+
+
+  // store texts in files statically
+    // need validation before store
+      // validation: ignore white space[x], and comments
+      // `//...` and `/*...*/` --> delete comment first and implement removeSpaces() function
+
+
+  // compare two diff variable to see if each index has identical character
+
+
+  // if all the same, true
+  // otherwise, false
+
+
